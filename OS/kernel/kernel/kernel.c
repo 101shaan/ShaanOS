11f582702fca6868403dd65a3ea4fcb98c3e4612
101shaan <lgs22sisos@lsf.org>
2025-06-16 12:41:42 +0100

    first commit

---
+++ OS/kernel/kernel/kernel.c
@@ -0,0 +1,35 @@
+#include<stdint.h> 
+#include "dadio.h"
+#include "phymem.h"
+#include "virtmem.h"
+#include "timer.h"
+#include "keyboard.h"
+#include "hal.h"
+#include "inthandling.h"
+#include "ATA.h"
+#include "FAT12.h"
+
+extern void kshell();
+extern void refresh_stack();
+uint8_t array[512];
+void initialize_all(uint32_t mmapsize);
+void kmain(uint32_t mmapsize,uint32_t data_sect,uint32_t root_sect,uint32_t fat_sect)
+{
+	gdt_init();
+	pmmngr_init(mmapsize); //Uses 0x1000... Don't remove identity map before that
+	vmmngr_init();  //Sets recursive map, remaps stack and vidmem
+	clear();
+
+	refresh_stack(); 
+	remove_identity_map();
+
+	interrupt_init();
+	kbc_init();
+	set_timer(0xffff);
+
+	kshell();
+
+	clear_interrupts();
+	kernel_wait();
+}
+
