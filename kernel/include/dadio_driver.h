cf976d218c649067f424079f4c44f72516cd2d00
101shaan <lgs22sisos@lsf.org>
2025-08-09 14:48:20 +0100

    dadio: include legacy dadio.h in dadio_driver.h for now

--- kernel/include/dadio_driver.h
+++ kernel/include/dadio_driver.h
@@ -2,17 +2,9 @@
 #define DADIO_DRIVER_H
 #include <stdint.h>
 
-// VGA color enum and function declarations as before
+// Provide declarations via legacy header for now.
+#include "dadio.h"
 
-void monitor_puts(char* Message);
-void printhex (uint32_t input);
-void printint (uint32_t input);
-void putc(char input);
-void set_fg_color(int color);
-void set_bg_color(int color);
-void clear();
-char get_monitor_char();
-uint32_t get_cursor();
-void set_cursor(uint32_t);
+// The above include provides the VGA color enum and function prototypes.
 
 #endif
