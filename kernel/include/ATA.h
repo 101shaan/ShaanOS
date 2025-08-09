7f460285e15da18d028dcbf5ceb66638cf3beb9b
101shaan <lgs22sisos@lsf.org>
2025-08-09 14:50:21 +0100

    ata: include stdint in deprecated ATA.h for prototypes

--- kernel/include/ATA.h
+++ kernel/include/ATA.h
@@ -1,6 +1,7 @@
 #ifndef ATA_H
 #define ATA_H
 // This file is deprecated. Use ata_driver.h instead.
+#include <stdint.h>
 void read_sectors_ATA_PIO(uint32_t target_address, uint32_t LBA, uint8_t sector_count);
 
 #endif
