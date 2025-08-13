7c1aed2dc46912f6bb16403d1a8c72b5a528526c
101shaan <lgs22sisos@lsf.org>
2025-08-13 17:14:46 +0100

    console: simplify dadio_driver.h to include legacy dadio.h explicitly

--- kernel/include/dadio_driver.h
+++ kernel/include/dadio_driver.h
@@ -1,10 +1,6 @@
 #ifndef DADIO_DRIVER_H
 #define DADIO_DRIVER_H
 #include <stdint.h>
-
-// Provide declarations via legacy header for now.
-#include "dadio.h"
-
-// The above include provides the VGA color enum and function prototypes.
+#include "dadio.h" // Provides enum vga_color and prototypes
 
 #endif
