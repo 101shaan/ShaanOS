ce7d9cb2749aba4737caad797ce16a38ed5181ef
101shaan <lgs22sisos@lsf.org>
2025-08-13 22:51:47 +0100

    feat(exceptions): add minimal ISR stubs for CPU exceptions 0-19 and pop error codes where required

--- kernel/hal/interruptstubs.asm
+++ kernel/hal/interruptstubs.asm
@@ -1,3 +1,24 @@
+global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
+global isr13
+global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+
 global isr32
 global isr33
 global isr34
@@ -12,12 +33,124 @@ global isr47
 
 
 ;These are C handlers
+extern default_handler
 extern timer_handler
 extern keyboard_handler
 
 
 extern send_EOI_master
 extern send_EOI_slave
+
+; ---------------------------------------------------------------------
+; Minimal exception stubs (0-19) -> call C default_handler
+; ---------------------------------------------------------------------
+isr0:
+	pusha
+	call default_handler
+	popa
+	iret
+isr1:
+	pusha
+	call default_handler
+	popa
+	iret
+isr2:
+	pusha
+	call default_handler
+	popa
+	iret
+isr3:
+	pusha
+	call default_handler
+	popa
+	iret
+isr4:
+	pusha
+	call default_handler
+	popa
+	iret
+isr5:
+	pusha
+	call default_handler
+	popa
+	iret
+isr6:
+	pusha
+	call default_handler
+	popa
+	iret
+isr7:
+	pusha
+	call default_handler
+	popa
+	iret
+isr8:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr9:
+	pusha
+	call default_handler
+	popa
+	iret
+isr10:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr11:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr12:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr13:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr14:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr15:
+	pusha
+	call default_handler
+	popa
+	iret
+isr16:
+	pusha
+	call default_handler
+	popa
+	iret
+isr17:
+	pusha
+	call default_handler
+	popa
+	add esp, 4 ; pop error code
+	iret
+isr18:
+	pusha
+	call default_handler
+	popa
+	iret
+isr19:
+	pusha
+	call default_handler
+	popa
+	iret
 
 isr32:
 	pusha
